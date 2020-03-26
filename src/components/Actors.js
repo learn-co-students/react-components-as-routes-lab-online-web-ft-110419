7f369858535fea86918072386603084dc401b75f
Gilblasse <gilblasse@gmail.com>
2020-03-25 23:33:03 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
+  const actorsList = () => {
+    return actors.map(({ name, movies }) => {
+      return (
+        <div key={name} className="actor">
+          <h3>Name: {name} </h3>
+          <p>Movies: </p>
+          <ul>{moviesList(movies)}</ul>
+        </div>
+      );
+    });
+  };
+
+  const moviesList = movies =>
+    movies.map(movie => <li key={movie}>{movie}</li>);
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      <div>{actorsList()}</div>
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  const movieList = () => {
+    return movies.map(({ title, time, genres }) => {
+      return (
+        <li key={title}>
+          <h3>Name: {title} </h3>
+          <p>Time: {time}</p>
+          <p>Genres: </p>
+          <ul>{genreList(genres)}</ul>
+        </li>
+      );
+    });
+  };
+
+  const genreList = genres => genres.map(genre => <li key={genre}>{genre}</li>);
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <ul>{movieList()}</ul>
     </div>
   );
 };
