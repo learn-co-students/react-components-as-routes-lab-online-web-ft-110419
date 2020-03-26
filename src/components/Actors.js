933668d1af7bfe550ba1ed0746a1457c96a5fe04
BrandonMWeaver <a7xncob@gmail.com>
2020-03-26 18:19:09 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => {
+        return (
+          <div key={index}>
+            <h3>{actor.name}</h3>
+            <ul>
+              {actor.movies.map((movie, index) => {
+                return (
+                  <li key={index}>{movie}</li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
