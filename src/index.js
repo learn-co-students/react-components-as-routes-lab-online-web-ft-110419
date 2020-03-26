cb167d1a7beff7dc4ee5e2deae681771d0ecadf5
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-26 15:14:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
 
+// index.js loads App as the top level component.
+
 render (
   <App />,
   document.getElementById('root')
